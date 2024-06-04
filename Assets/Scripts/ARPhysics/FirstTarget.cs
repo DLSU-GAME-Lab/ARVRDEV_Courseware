a91d691c8ca136f3024f883079c23de4b77afbc5
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2024-06-04 11:10:18 +0800

    Re-integrated AR physics and AR video playback.

---
+++ Assets/Scripts/ARPhysics/FirstTarget.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class FirstTarget : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        
+    }
+
+    private void OnDestroy() {
+        
+    }
+
+    public void OnTargetFound() {
+        EventBroadcaster.Instance.PostEvent(EventNames.ARPhysicsEvents.ON_FIRST_TARGET_SCAN);
+    }
+}
