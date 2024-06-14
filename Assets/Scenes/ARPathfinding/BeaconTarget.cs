6c54b6937a2e923e629e13505d92c9ad4f5223a0
Lanz Kendall Y. Lim <114926584+LanzL20@users.noreply.github.com>
2024-06-14 10:41:40 +0800

    PathFinding and WreckingBall
    Initial Commit

---
+++ Assets/Scenes/ARPathfinding/BeaconTarget.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Remoting.Messaging;
+using System.Runtime.Remoting.Services;
+using UnityEngine;
+using Vuforia;
+
+public class BeaconTarget : MonoBehaviour//, ItrackableEventHandler
+{
+    public const string BEACON_POSITION_KEY = "BEACON_POSITION_KEY";
+    //[SerializeField]
+    //private PlatformTarget platformtarget;
+    [SerializeField]
+    private Camera arCamera;
+    //private TrackableBehaviour mTrackableBehavior;
+    // Start is called before the first frame update
+    void Start()
+    {
+        /*mTrackableBehavior = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehavior)
+        {
+            mTrackableBehavior.RegisterTrackableEventHandler(this);
+        }*/
+        EventBroadcaster.Instance.AddObserver(EventNames.ARPathFindEvents.ON_BEACON_DETECTED, this.FoundCaller);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void FoundCaller()
+    {
+        Invoke("OnBeaconFound", 0.2f);
+    }
+
+    private void OnBeaconFound()
+    {
+        Debug.Log("Hilo ther");
+        
+
+        Parameters parameters = new Parameters();
+        Vector3 trackedPos = this.TranslateBeaconPosition();
+        parameters.PutObjectExtra(BEACON_POSITION_KEY, trackedPos);
+        EventBroadcaster.Instance.PostEvent(EventNames.ARPathFindEvents.ON_BEACON_DETECTED, parameters);
+    }
+
+    private Vector3 TranslateBeaconPosition()
+    {
+        
+        Debug.Log("Hilo ther again");
+        Ray ray= this.arCamera.ScreenPointToRay(this.arCamera.WorldToScreenPoint(this.transform.position));
+        Debug.Log("Hilo there again again");
+        Debug.DrawRay(ray.origin, ray.direction, Color.red);
+
+        RaycastHit hit;
+        Vector3 hitPos = Vector3.zero;
+        if(Physics.Raycast(ray, out hit))
+        {
+            hitPos=hit.point;
+            Debug.Log("Hit pos: "+hitPos+" at object: "+ hit.transform.gameObject.name);
+        }
+        return hitPos;  
+    }
+
+
+}
