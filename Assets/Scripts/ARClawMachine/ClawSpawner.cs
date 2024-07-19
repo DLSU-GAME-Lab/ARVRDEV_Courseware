527b9a10e652193a1c51e91929c731bb533ebded
Lanz Kendall Y. Lim <114926584+LanzL20@users.noreply.github.com>
2024-07-19 12:00:36 +0800

    Final Revisions to Claw Machine
    fixed speed
    anchor score top left
    fixed stick prize prize collider

--- Assets/Scripts/ARClawMachine/ClawSpawner.cs
+++ Assets/Scripts/ARClawMachine/ClawSpawner.cs
@@ -20,7 +20,7 @@ public class Clawspawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        PrizeSpawner();
+
     }
 
     // Update is called once per frame
